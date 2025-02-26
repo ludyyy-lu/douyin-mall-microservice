6acb79ca4e2598272dff9884b980a14d3769f74a
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-02-26 11:42:43 +0800

    删除部分冲突文件，修改部分文件内容

--- app/frontend/biz/handler/cart/cart_service_test.go
+++ app/frontend/biz/handler/cart/cart_service_test.go
@@ -12,29 +12,22 @@ import (
 func TestGetCart(t *testing.T) {
 	h := server.Default()
 	h.GET("/cart", GetCart)
-	path := "/cart"                                           // todo: you can customize query
-	body := &ut.Body{Body: bytes.NewBufferString(""), Len: 1} // todo: you can customize body
-	header := ut.Header{}                                     // todo: you can customize header
+	path := "/cart"                                           
+	body := &ut.Body{Body: bytes.NewBufferString(""), Len: 1} 
+	header := ut.Header{}                                     
 	w := ut.PerformRequest(h.Engine, "GET", path, body, header)
 	resp := w.Result()
 	t.Log(string(resp.Body()))
 
-	// todo edit your unit test.
-	// assert.DeepEqual(t, 200, resp.StatusCode())
-	// assert.DeepEqual(t, "null", string(resp.Body()))
 }
 
 func TestAddCartItem(t *testing.T) {
 	h := server.Default()
 	h.POST("/cart", AddCartItem)
-	path := "/cart"                                           // todo: you can customize query
-	body := &ut.Body{Body: bytes.NewBufferString(""), Len: 1} // todo: you can customize body
-	header := ut.Header{}                                     // todo: you can customize header
+	path := "/cart"                                           
+	body := &ut.Body{Body: bytes.NewBufferString(""), Len: 1} 
+	header := ut.Header{}                                     
 	w := ut.PerformRequest(h.Engine, "POST", path, body, header)
 	resp := w.Result()
 	t.Log(string(resp.Body()))
-
-	// todo edit your unit test.
-	// assert.DeepEqual(t, 200, resp.StatusCode())
-	// assert.DeepEqual(t, "null", string(resp.Body()))
 }
