34c34d292b4379798072847165c6974a3f8f3ba2
powerli2002 <845160705@qq.com>
2025-03-02 16:42:29 +0800

    update go.mod

--- app/order/core/db.go
+++ app/order/core/db.go
@@ -1,11 +1,11 @@
 package core
 
-import (
-	"github.com/All-Done-Right/douyin-mall-microservice/app/order/biz/dal/mysql"
-	"github.com/All-Done-Right/douyin-mall-microservice/app/order/biz/dal/redis"
-)
+// import (
+// 	"github.com/All-Done-Right/douyin-mall-microservice/app/order/biz/dal/mysql"
+// 	"github.com/All-Done-Right/douyin-mall-microservice/app/order/biz/dal/redis"
+// )
 
-func InitDB() {
-	mysql.InitMysql()
-	redis.InitRedis()
-}
+// func InitDB() {
+// 	mysql.InitMysql()
+// 	redis.InitRedis()
+// }
