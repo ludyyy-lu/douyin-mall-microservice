24a5648f926c8a4f59ab8344564e73184cb8b1c9
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-03-05 16:06:37 +0800

    修改cart.sql注释

--- app/cart/script/cart.sql
+++ app/cart/script/cart.sql
@@ -1,13 +1,2 @@
 -- Active: 1740897367239@@127.0.0.1@3306@gorm
 CREATE DATABASE IF NOT EXISTS cart;
-
-/*create table cart if not exists cart
-(
-    id         int auto_increment,
-    user_id    int      not null,
-    product_id int      not null,
-    qty        int      not null,
-    created_at datetime not null default current_timestamp,
-    updated_at datetime not null default current_timestamp on update current_timestamp,
-    constraint cart_pk primary key (id)
-);*/
